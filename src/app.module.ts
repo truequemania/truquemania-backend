5f431e3ab51558a1f356a6eb4751529e93c99560
Gustavo <truequemania.cl@gmail.com>
2024-12-19 21:23:29 -0400

    Cambiando el puerto

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ console.log(process.env.DB_HOST);
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 21747,
+      port: 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
